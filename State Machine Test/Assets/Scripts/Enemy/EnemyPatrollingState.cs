878a2767057337a0c2f41d46fa5706c5df4f0ea4
Michael Smidt <michael.smidt@hva.nl>
2023-04-17 17:10:12 +0200

    Added a raycast that detects the player

--- State Machine Test/Assets/Scripts/Enemy/EnemyPatrollingState.cs
+++ State Machine Test/Assets/Scripts/Enemy/EnemyPatrollingState.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 public class EnemyPatrollingState : EnemyBaseState
 {
     float moveSpeed = 10;
+    Vector3 fwd;
+    Ray ray;
+    int layerMask = 1 << 3;
     Rigidbody rb;
     float countdown;
     public override void EnterState(EnemyStateManager enemy)
     {
+        fwd = enemy.transform.forward;
         countdown = 5;
         rb = enemy.GetComponent<Rigidbody>();
         rb.constraints = RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationX;
@@ -15,6 +19,14 @@ public class EnemyPatrollingState : EnemyBaseState
 
     public override void UpdateState(EnemyStateManager enemy)
     {
+        ray = new Ray(enemy.transform.position + new Vector3(0, -0.5f, 0), fwd);
+        if(Physics.Raycast(ray, 10, layerMask))
+        {
+            Debug.Log("I see the player!");
+        }
+
+        
+
         rb.velocity = enemy.transform.forward * moveSpeed;
         if(countdown >= 0)
         {
@@ -34,8 +46,9 @@ public class EnemyPatrollingState : EnemyBaseState
     {
         GameObject other = collision.gameObject;
         if(other.tag == "Wall")
-        {           
+        {            
             enemy.transform.Rotate(0, -180, 0);
+            fwd = enemy.transform.forward;
             rb.velocity = enemy.transform.forward * moveSpeed;
         }
     }
