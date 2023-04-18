a77bdca6d24c2d5fb155f2152b54136478974dcb
Michael Smidt <michael.smidt@hva.nl>
2023-04-18 13:44:46 +0200

    Added basic player controller for testing

---
+++ State Machine Test/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    CharacterController cc;
+    Vector3 movementSpeed;
+    float playerSpeed = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cc = GetComponent<CharacterController>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        cc.Move(move * Time.deltaTime * playerSpeed);
+        
+    }
+}
